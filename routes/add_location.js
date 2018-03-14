547ee02a1b010867b972115ed70dcb6cdc212ba7
anisham197 <anisham197@gmail.com>
2018-03-14 12:02:24 +0530

    Write locations to firestore

--- routes/add_location.js
+++ routes/add_location.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require('firebase-admin');
+var db = admin.firestore();
+
 router.get('/', function(req, res, next) {
   res.render('home/add_location_form', { title: 'Location Form' });
+});
+
+
+router.post('/', function(req, res, next) {
+
+  var userID = "";
+
+  var data = {
+    name: req.body.name,
+    userID: userID,
+    metadata:  {
+      city: req.body.city
+    }
+  };
+  var setDoc = db.collection('locations').doc().set(data);
+  res.redirect( '/home');
 });
 
 module.exports = router;
