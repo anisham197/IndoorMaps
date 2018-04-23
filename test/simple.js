cf29467c23734221c510907ab0af3c2a79217d13
akkisho <akkisho@amazon.com>
2018-04-23 23:38:48 +0530

    added tests via chai and mocha

---
+++ test/simple.js
@@ -0,0 +1,33 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('Laberinto', () => {
+  describe('GET /', () => {
+      it('it should GET the homepage', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+              done();
+            });
+      });
+  });
+
+  describe('GET /locations', () => {
+      it('should GET the locations page if authed else redirect to page', (done) => {
+        chai.request(server)
+            .get('/locations')
+            .end((err, res) => {
+                res.should.have.status(200);
+              done();
+            });
+      });
+  });
+});
