ad976cbf39038a7f9457e9b5247d69a179225860
akkisho <akkisho@amazon.com>
2018-04-26 00:12:35 +0530

    modified scripts

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+. /home/ubuntu/.nvm/nvm.sh
 . /home/ubuntu/.bashrc
 
 cd /home/ubuntu/app
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
+set -e
 . /home/ubuntu/.nvm/nvm.sh
-. /home/ubuntu/.profile
 . /home/ubuntu/.bashrc
 
 cd /home/ubuntu/app
