070789940eb744ad57012b4fef40b3fcf1e91fcb
akkisho <akkisho@amazon.com>
2018-04-26 02:10:05 +0530

    modified scripts

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -10,7 +10,7 @@ server {
     server_name beta.vendor.maps.goflo.in;
 
     location / {
-        proxy_set_header   X-Forwarded-For $remote_addr;
+        proxy_set_header   X-Forwarded-For \$remote_addr;
         proxy_set_header   Host $http_host;
         proxy_pass         "http://127.0.0.1:3000";
     }
