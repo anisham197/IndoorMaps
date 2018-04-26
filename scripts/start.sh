2ef00dfa5b50e7d90e9d220e4dbda7ed23e7bd28
akkisho <akkisho@amazon.com>
2018-04-26 19:49:33 +0530

    modified scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 echo "Step1: '$NVM_DIR'"
+. $HOME/.bashrc
+echo "Step2: '$NVM_DIR'"
 if [ -f $HOME/.nvm/nvm.sh ]; then
     . $HOME/.nvm/nvm.sh
 fi
-echo "Step2: '$NVM_DIR'"
-. $HOME/.profile
 echo "Step3: '$NVM_DIR'"
-. $HOME/.bashrc
+. $HOME/.profile
 echo "Step4: '$NVM_DIR'"
 
 cd /home/ubuntu/app
